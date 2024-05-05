30eed1768a46a4f818163f81721ce4c7d1aaecf3
Jason Tam <jason6809@gmail.com>
2024-05-06 00:32:11 +0800

    Added test case.

--- tests/Unit/GenerateSSLTest.php
+++ tests/Unit/GenerateSSLTest.php
@@ -2,21 +2,35 @@
 
 namespace Jason6809\LaravelOpenssl\Tests\Unit;
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\File;
 use Jason6809\LaravelOpenssl\Tests\TestCase;
 
 class GenerateSSLTest extends TestCase
 {
-    public function the_command_successfully_generate_cert_and_key()
+    /** @test */
+    public function generate_ssl()
     {
+        if (file_exists('example.test.key')) {
+            unlink('example.test.key');
+        }
+
+        if (file_exists('example.test.crt')) {
+            unlink('example.test.crt');
+        }
+
         $this->artisan('openssl:generate')
             ->expectsQuestion('Enter the Common Name (CN) for the SSL certificate.', 'example.test')
             ->expectsQuestion('Enter SAN hostnames. (In comma-separated, e.g: example.com,www.example.com,...)', 'example.test,www.example.test')
-            ->expectsOutput('SSL Certificate generated successfully.')
             ->assertExitCode(0);
 
-        $this->assertTrue(file_exists(base_path('example.test.key')));
-        $this->assertTrue(file_exists(base_path('example.test.crt')));
+        $this->assertTrue(file_exists('example.test.key'));
+        $this->assertTrue(file_exists('example.test.crt'));
+
+        if (file_exists('example.test.key')) {
+            unlink('example.test.key');
+        }
+
+        if (file_exists('example.test.crt')) {
+            unlink('example.test.crt');
+        }
     }
 }
